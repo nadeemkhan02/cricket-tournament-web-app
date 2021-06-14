be7bd3ebac66c486cfcfd73553e2ab4158a2a4a6
nadeemkhan02 <nadeem@billeasy.in>
2021-06-14 10:35:41 +0530

    matches page updated

--- src/Pages/Matches.jsx
+++ src/Pages/Matches.jsx
@@ -1,11 +1,36 @@
 import React from "react";
+import Head from "../Components/Head";
 
 const Matches = () => {
-    return ( 
+    const matchData = require("../Services/Matches.json")
+    const {matches} = matchData
+    return (
         <>
-            <h1>Matches Data</h1>
+            <Head title="All Matches" />
+          <div style={{marginTop:"20px", alignItems:"center"}} className="card-container">
+            <table style={{textAlign:"center"}} class="table table-hover table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">Match No:</th>
+                        <th scope="col">Match:</th>
+                        <th scope="col">Ground:</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        matches.map((val) => (
+                            <tr>
+                                <th scope="row">{matches.indexOf(val)+1}</th>
+                                <td>{val.team1} VS {val.team2}</td>
+                                <td>{val.venue}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+            </div>
         </>
-     );
+    );
 }
- 
+
 export default Matches;
